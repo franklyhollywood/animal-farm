900df5822d83505c0024948ddc2be79e4e99fa20
Frank Hollywood <github@srvsync.com>
2021-07-15 10:42:38 -0700

    Created animal farm

--- app.js
+++ app.js
@@ -2,7 +2,31 @@
 
 // initialize state
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+// set event listeners
+// get user input
+// use user input to update state
+// update DOM to reflect the new state
+
+const h1El = document.getElementById('header');
+const horseEl = document.getElementById('horse-image');
+const dogEl = document.getElementById('dog-image');
+const catEl = document.getElementById('cat-image');
+const horseSound = document.getElementById('horse-sound');
+const dogSound = document.getElementById('dog-sound');
+const catSound = document.getElementById('cat-sound');
+
+console.log(h1El, horseEl, dogEl, catEl, horseSound, dogSound, catSound);
+horseEl.addEventListener('click', () => {
+	horseSound.play();
+	h1El.textContent = 'horse';
+});
+
+dogEl.addEventListener('click', () => {
+	dogSound.play();
+	h1El.textContent = 'dog';
+});
+
+catEl.addEventListener('click', () => {
+	catSound.play();
+	h1El.textContent = 'cat';
+});
